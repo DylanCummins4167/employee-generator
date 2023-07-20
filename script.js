a0cbda74a2635bdd14af8a28a35076a25f4436f2
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-07-20 01:31:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const readline = require('readline');
 const sqlite3 = require('sqlite3').verbose();
+
+// Function to create tables if they don't exist
+function createTables(db) {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS departments (
+      id INTEGER PRIMARY KEY,
+      name TEXT
+    )
+  `);
